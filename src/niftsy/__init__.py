3abbaa02d50be3ab29eb48db1b85ee427cae9b18
Gabriel Díaz <gd27@rice.edu>
2026-08-10 19:26:50 -0500

    Public exports added very simple.

--- src/niftsy/__init__.py
+++ src/niftsy/__init__.py
@@ -1,3 +1,8 @@
-from niftsy.pipeline import GenerationResult, SyntheticDataGenerator, generate_synthetic_dataset
+from niftsy.pipeline import SyntheticDataGenerator, generate_synthetic_dataset, GenerationResult
+from niftsy.config import GenerationConfig, TabularConfig, LLMConfig, PromptConfig
+from niftsy.exceptions import NiftsyError
 
-__all__ = ["GenerationResult", "SyntheticDataGenerator", "generate_synthetic_dataset"]
+__all__ = [
+    "SyntheticDataGenerator", "generate_synthetic_dataset", "GenerationResult",
+    "GenerationConfig", "TabularConfig", "LLMConfig", "PromptConfig", "NiftsyError",
+]
